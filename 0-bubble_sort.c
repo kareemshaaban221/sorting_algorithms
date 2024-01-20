ee3cde9eb408f57c942083dd569e8c502b3bd72e
kareemshaaban221 <kareemshaaban221gmail.com>
2024-01-20 23:46:58 +0200

    test

---
+++ 0-bubble_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+ * swap - swaping two integers
+ * @a: pointer to the first integer
+ * @b: pointer to the second integer
+ * Return: void
+*/
+void swap(int *a, int *b)
+{
+int tmp = *a;
+*a = *b;
+*b = tmp;
+}
+
+/**
+ * bubble_sort - implementation of bubble sort
+ * @array: unsorted array
+ * @size: array size
+ * Return: void
+*/
+void bubble_sort(int *array, size_t size)
+{
+size_t i = 0, j = 0;
+
+while (i < size) {
+for (j = 0; j < size - i - 1; j++)
+if (array[j] > array[j + 1])
+{
+swap(&array[j], &array[j + 1]);
+print_array(array, size);
+}
+i++;
+}
+}
