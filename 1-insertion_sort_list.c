733e1cdd02f3cf55dbc55a737ce00a4f5a40bfca
kareemshaaban221 <kareemshaaban221gmail.com>
2024-01-22 02:20:27 +0200

    test

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,19 +8,19 @@
 */
 void swap(listint_t *a, listint_t *b, listint_t **list)
 {
-    listint_t *nxtTmp = b->next;
-    listint_t *preTmp = a->prev;
+listint_t *nxtTmp = b->next;
+listint_t *preTmp = a->prev;
 
-    if (preTmp)
-        preTmp->next = b;
-    else
-        *list = b;
-    if (nxtTmp)
-        nxtTmp->prev = a;
-    a->next = nxtTmp;
-    a->prev = b;
-    b->next = a;
-    b->prev = preTmp;
+if (preTmp)
+preTmp->next = b;
+else
+*list = b;
+if (nxtTmp)
+nxtTmp->prev = a;
+a->next = nxtTmp;
+a->prev = b;
+b->next = a;
+b->prev = preTmp;
 }
 
 /**
@@ -30,32 +30,32 @@ void swap(listint_t *a, listint_t *b, listint_t **list)
 */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *i = *list, *j = NULL, *k = NULL, *tmp = NULL;
-    short firstIteration = 1;
+listint_t *i = *list, *j = NULL, *k = NULL, *tmp = NULL;
+short firstIteration = 1;
 
-    if (!list)
-        return;
+if (!list || !(*list)->next)
+return;
 
-    while (i)
-    {
-        k = i->prev;
-        j = i;
-        while (k && k->n > j->n)
-        {
-            swap(k, j, list);
-            if (firstIteration)
-            {
-                i = k;
-                firstIteration = 0;
-            }
-            tmp = k;
-            k = j;
-            j = tmp;
-            j = j->prev;
-            k = k->prev;
-            print_list(*list);
-        }
-        firstIteration = 1;
-        i = i->next;
-    }
+while (i)
+{
+k = i->prev;
+j = i;
+while (k && k->n > j->n)
+{
+swap(k, j, list);
+if (firstIteration)
+{
+i = k;
+firstIteration = 0;
+}
+tmp = k;
+k = j;
+j = tmp;
+j = j->prev;
+k = k->prev;
+print_list(*list);
+}
+firstIteration = 1;
+i = i->next;
+}
 }
