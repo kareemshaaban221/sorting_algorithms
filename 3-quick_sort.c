c0ea2f0c990dae8f00b043f7a4ee6f15c4ef4f41
kareemshaaban221 <kareemshaaban221gmail.com>
2024-01-23 21:51:48 +0200

    test

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -5,7 +5,7 @@
  * @a: pointer to the first integer
  * @b: pointer to the second integer
  * Return: void
-*/
+ */
 void swap(int *a, int *b)
 {
 int tmp = *a;
@@ -20,14 +20,15 @@ int tmp = *a;
  * @h: high
  * @size: size
  * Return: pivot
-*/
+ */
 size_t lomuto_partition(int *array, size_t l, size_t h, size_t size)
 {
 size_t p = h, i = l, j = l;
 
 while (i < h)
 {
-if (array[i] >= array[p]);
+if (array[i] >= array[p])
+;
 else
 {
 if (array[i] != array[j])
@@ -45,7 +46,7 @@ swap(&array[j], &array[p]);
 print_array(array, size);
 }
 p = j;
-return p;
+return (p);
 }
 
 /**
@@ -55,7 +56,7 @@ return p;
  * @h: high
  * @size: size
  * Return: void
-*/
+ */
 void quick_sort_recursive(int *array, size_t l, size_t h, size_t size)
 {
 size_t p = 0;
@@ -63,6 +64,7 @@ size_t p = 0;
 if (l >= h)
 return;
 p = lomuto_partition(array, l, h, size);
+if (p < size - 1)
 quick_sort_recursive(array, p + 1, h, size);
 if (p > 0)
 quick_sort_recursive(array, l, p - 1, size);
@@ -73,7 +75,7 @@ quick_sort_recursive(array, l, p - 1, size);
  * @array: array of integers
  * @size: number of elements
  * Return: void
-*/
+ */
 void quick_sort(int *array, size_t size)
 {
 if (size == 1)
