5448665dc9cb4be7ebf70ad452a1080c9bb9b95c
kareemshaaban221 <kareemshaaban221gmail.com>
2024-01-23 03:09:48 +0200

    test

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -14,24 +14,27 @@ int tmp = *a;
 }
 
 /**
- * 
+ * selection_sort - selection sort function
+ * @array: pointer to array of integers
+ * @size: size of array
+ * Return: void
 */
 void selection_sort(int *array, size_t size)
 {
-    size_t min = 0, i = 0, j = 0;
-    
-    for (i = 0; i < size; i++)
-    {
-        min = i;
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[min])
-                min = j;
-        }
-        if (i != min)
-        {
-            swap(&array[i], &array[min]);
-            print_array(array, size);
-        }
-    }
+size_t min = 0, i = 0, j = 0;
+
+for (i = 0; i < size; i++)
+{
+min = i;
+for (j = i + 1; j < size; j++)
+{
+if (array[j] < array[min])
+min = j;
+}
+if (i != min)
+{
+swap(&array[i], &array[min]);
+print_array(array, size);
+}
+}
 }
