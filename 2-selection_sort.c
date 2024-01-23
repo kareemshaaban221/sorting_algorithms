946c60be1f9e496455b5c7d5d0aab74f2a2d2ea6
kareemshaaban221 <kareemshaaban221gmail.com>
2024-01-23 03:08:40 +0200

    test

---
+++ 2-selection_sort.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+
+/**
+ * swap - swaping two integers
+ * @a: pointer to the first integer
+ * @b: pointer to the second integer
+ * Return: void
+*/
+void swap(int *a, int *b)
+{
+int tmp = *a;
+*a = *b;
+*b = tmp;
+}
+
+/**
+ * 
+*/
+void selection_sort(int *array, size_t size)
+{
+    size_t min = 0, i = 0, j = 0;
+    
+    for (i = 0; i < size; i++)
+    {
+        min = i;
+        for (j = i + 1; j < size; j++)
+        {
+            if (array[j] < array[min])
+                min = j;
+        }
+        if (i != min)
+        {
+            swap(&array[i], &array[min]);
+            print_array(array, size);
+        }
+    }
+}
